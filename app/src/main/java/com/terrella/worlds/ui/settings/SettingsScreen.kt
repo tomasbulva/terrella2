47ee9db869ffedcefa47927f01321cf3539a7bf5
Tomas Bulva <tomasbulva@users.noreply.github.com>
2026-09-11 18:49:06 +0000

    fix: clickable + Icon imports

--- app/src/main/java/com/terrella/worlds/ui/settings/SettingsScreen.kt
+++ app/src/main/java/com/terrella/worlds/ui/settings/SettingsScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -23,6 +24,7 @@ import androidx.compose.material.icons.filled.Public
 import androidx.compose.material.icons.filled.Straighten
 import androidx.compose.material.icons.filled.Title
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
