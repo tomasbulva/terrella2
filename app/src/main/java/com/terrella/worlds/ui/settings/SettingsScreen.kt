59f3515048864ed6b6c170b4060f3783663d4256
Tomas Bulva <tomasbulva@users.noreply.github.com>
2026-09-11 20:07:00 +0000

    fix: dedupe imports

--- app/src/main/java/com/terrella/worlds/ui/settings/SettingsScreen.kt
+++ app/src/main/java/com/terrella/worlds/ui/settings/SettingsScreen.kt
@@ -27,11 +27,9 @@ import androidx.compose.material.icons.filled.Public
 import androidx.compose.material.icons.filled.Straighten
 import androidx.compose.material.icons.filled.Title
 import androidx.compose.foundation.Image
-import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
