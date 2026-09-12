b6041816b4c561d33f465b12a55c4e0e97577a3d
Tomas Bulva <tomasbulva@users.noreply.github.com>
2026-09-12 11:00:47 +0000

    fix: lifecycle import in detail screen

--- app/src/main/java/com/terrella/worlds/ui/detail/LocationDetailScreen.kt
+++ app/src/main/java/com/terrella/worlds/ui/detail/LocationDetailScreen.kt
@@ -32,7 +32,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsStateWithLifecycle
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
